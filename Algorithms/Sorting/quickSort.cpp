43aa510dfcec3be69651230cf9db201676b55046
Gunanka D <158302120+Gunanana@users.noreply.github.com>
2024-07-04 00:34:26 +0530

    Update quickSort.cpp
    hell yeah a better explained qs with vectors

--- Algorithms/Sorting/quickSort.cpp
+++ Algorithms/Sorting/quickSort.cpp
@@ -1,38 +1,39 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-void arrayPrinter(int arr[],int size){
-    for(int i=0;i<size;i++){
-        cout<<arr[i]<<" ";
+int partition(vector<int> &vec, int low, int high){
+    int i = low, j = high;
+    int pivot = vec[low]; //take low element as the pivot
+
+    while(i < j){
+        //if left part of the vector has numbers smaller than or equal to pivot, then ching ching they are in their correct position, hence move i till u find a number greater than the pivot
+        while(i<high && vec[i] <= pivot) i++;
+
+        //if the right part of the vector has numbers greater than the pivot, then they are in their correct position, hence decrease j till u find a number lesser than pivot
+        //and as for the >= and > controversy, >= will make the duplicates fall in the right partition, whereas the > will make the duplicates fall in the left side
+        //if u are taking first element as the pivot element, its better to throw dupes to the right partition and hence use >
+        while(j>low && vec[j] > pivot) j--;
+
+        //to handle few edge cases, swap iff i is still before j
+        if(i<j) swap(vec[j] , vec[i]);
     }
-    cout<<endl;
+
+    swap(vec[low] , vec[j]);
+    //why j?
+    //because j will be pointing to the number smaller than the pivot element and rest of the numbers after j will all be more than the pivot element (because this is ascending order)
+    return j;
 }
 
-int partition(int *arr,int s,int e){
-    int count=0;
-    for(int i=s+1;i<=e;i++) if(arr[s]>=arr[i]) count++;
-    int pivotIndex=s+count;
-    swap(arr[pivotIndex],arr[s]);
-    int i=s;
-    int j=e;
-    while(i<pivotIndex&&j>pivotIndex){
-        while(arr[i]<arr[pivotIndex])i++;
-        while(arr[j]>arr[pivotIndex])j--;
-        if((i<pivotIndex)&&(j>pivotIndex)) swap(arr[i],arr[j]);
-    }
-    return pivotIndex;
-}
 
-void quickSort(int *arr, int s,int e){
-    if(s>=e) return; //base condition
-    int p= partition(arr,s,e); 
-    quickSort(arr,s,p-1); //quicksort on the left array
-    quickSort(arr,p+1,e); //quicksort on the right array
+void quickSort(vector<int> &vec, int low, int high){
+    if(low>=high) return;
+    int pivot = partition(vec, low, high);
+    quickSort(vec, low, pivot-1);
+    quickSort(vec, pivot+1, high);
 }
 
 int main(){
-    int arr[10]={11,10,1,2,3,13,12,16,7,8};
-    arrayPrinter(arr,10);
-    quickSort(arr,0,9);
-    arrayPrinter(arr,10);
+    vector<int> vec = {765,4,65,6,778865,3,4,45,3,5,6,7};
+    quickSort(vec, 0 , vec.size()-1);
+    for(auto i : vec) cout<<i<<" ";
 }
